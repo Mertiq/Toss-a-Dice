5859636af7d7d16a0981485c386fce96afb2a655
Mert <mertiq@icloud.com>
2021-01-17 09:28:27 +0300

    first

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour
+{
+    Rigidbody rb;
+    public static Vector3 dVelocity;
+
+    public Text text;
+
+    public static float number = 0;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        dVelocity = rb.velocity;
+        
+        text.text = number.ToString();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = new Vector3(0, 4, 0);
+            rb.velocity = Vector3.zero;
+            float rotX = Random.Range(0, 361) % 90;
+            float rotY = Random.Range(0, 361) % 90;
+            float rotZ = Random.Range(0, 361) % 90;
+            transform.rotation = new Quaternion(rotX, rotY, rotZ, transform.rotation.w);
+            float dirX = Random.Range(0, 500);
+            float dirY = Random.Range(0, 500);
+            float dirZ = Random.Range(0, 500);
+            rb.AddForce(transform.up * 500);
+            rb.AddTorque(dirX, dirY, dirZ);
+
+        }
+    }
+}
---
+++ Assets/Scripts/DiceNumberCheck.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceNumberCheck : MonoBehaviour
+{
+    Vector3 diceVelocity;
+    void FixedUpdate()
+    {
+        diceVelocity = Dice.dVelocity;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(diceVelocity == Vector3.zero)
+        {
+            Debug.Log(other.gameObject.name);
+            switch (other.gameObject.name)
+            {
+                case "side 1":
+                    Dice.number = 6;
+                    break;
+                case "side 2":
+                    Dice.number = 5;
+                    break;
+                case "side 3":
+                    Dice.number = 4;
+                    break;
+                case "side 4":
+                    Dice.number = 3;
+                    break;
+                case "side 5":
+                    Dice.number = 2;
+                    break;
+                case "side 6":
+                    Dice.number = 1;
+                    break;
+            }
+        }
+    }
+}
